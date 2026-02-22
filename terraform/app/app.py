b247c26565f1b94df6b793bec83d7bc5a670fb64
devautomation <deepak_kr92@icloud.com>
2026-02-22 19:02:25 +0530

    small-fix

--- terraform/app/app.py
+++ terraform/app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
 
+from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "🚀 DevOps Production EKS Automation by Deepak is LIVE!"
 
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+    
